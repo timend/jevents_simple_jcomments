4c07ffdc473342e5de39b48928832ef1fe1292d6
Tim Enderling <t.enderling@gmx.de>
2015-09-28 21:53:37 +0200

    Allow displaying comments count as custom field.

--- simpleJComments.php
+++ simpleJComments.php
@@ -6,41 +6,56 @@ jimport( 'joomla.plugin.plugin' );
 
 // Lets load the language file
 $lang = JFactory::getLanguage();
-$lang->load("plg_simple_jcomments", JPATH_ADMINISTRATOR);
+$lang->load("plg_jevents_simple_jcomments", JPATH_ADMINISTRATOR);
 
 class plgJEventsSimpleJComments extends JPlugin
 {
-	function onDisplayCustomFields(&$row){
-            if (file_exists(JPATH_SITE.'/components/com_jcomments/jcomments.php')) {
-                    require_once(JPATH_SITE.'/components/com_jcomments/jcomments.php');
-                    $row->_jcomments = JComments::showComments($row->rp_id(), 'com_jevents', $row->content());
-                    return $row->_jcomments;
-            }		
-	}	
+    function onDisplayCustomFields(&$row){        
+        if (file_exists(JPATH_SITE.'/components/com_jcomments/jcomments.php')) {
+            require_once(JPATH_SITE.'/components/com_jcomments/jcomments.php');
+            $row->_jcomments = JComments::showComments($row->rp_id(), 'com_jevents', $row->content());
+            $row->_jcommentsCount = "<div class=\"simple_jcomments_counter\">". 
+                        JComments::getCommentsCount($row->rp_id(), 'com_jevents'). " Kommentar(e)</div>";
+            return $row->_jcomments;
+        }		
+    }	
+
+    function onDisplayCustomFieldsMultiRow($rows)
+    {
+        //TODO: Optimize if necessary
+        foreach($rows as $row) {
+            $this->onDisplayCustomFields($row);
+        }
+    }
+
+    static function fieldNameArray($layout='detail')
+    {
+        if ($layout != "detail" && $layout != "list") {
+            return array();
+        }
         
-	static function fieldNameArray($layout='detail')
-	{
-            if ($layout != "detail")
-                    return array();
-            $labels = array();
-            $values = array();
-            $labels[] = JText::_("JEV_JCOMMENTS", true);
-            $values[] = "JEV_JCOMMENTS_ENABLE";
+        $labels = array();
+        $values = array();
+        $labels[] = JText::_("JEV_SIMPLE_JCOMMENTS_ENABLE", true);
+        $values[] = "JEV_SIMPLE_JCOMMENTS_ENABLE";
+        $labels[] = JText::_("JEV_SIMPLE_JCOMMENTS_COUNT", true);
+        $values[] = "JEV_SIMPLE_JCOMMENTS_COUNT";
 
-            $return = array();
-            $return['group'] = JText::_("JEV_JCOMMENTS", true);
-            $return['values'] = $values;
-            $return['labels'] = $labels;
+        $return = array();
+        $return['group'] = JText::_("JEV_SIMPLE_JCOMMENTS", true);
+        $return['values'] = $values;
+        $return['labels'] = $labels;
 
-            return $return;
+        return $return;
 
-	}
-        static function substitutefield($row, $code)
-	{
-            if ($code == "JEV_JCOMMENTS_ENABLE")
-            {
-                return $row->_jcomments;			
-            }
-	}
-
+    }
+    
+    static function substitutefield($row, $code)
+    {
+        if ($code == "JEV_SIMPLE_JCOMMENTS_ENABLE") {
+            return $row->_jcomments;			
+        } else if ($code == "JEV_SIMPLE_JCOMMENTS_COUNT") {
+            return $row->_jcommentsCount;		
+        }
+    }
 }
